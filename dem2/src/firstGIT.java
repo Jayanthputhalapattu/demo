e7ef9a4d9c0e12db921d782b805f9bceea8fdc00
Jayanth Puthalapattu <jayanthp17@gmail.com>
2020-02-09 09:14:32 +0530

    pushing into new branch

--- dem2/src/firstGIT.java
+++ dem2/src/firstGIT.java
@@ -7,6 +7,7 @@ public class firstGIT {
   System.out.println("THis is second commit");
   System.out.println("This is thrd");
   System.out.println("This is new branch");
+  System.out.println("Pushing into new branch");
 	}
 
 }
