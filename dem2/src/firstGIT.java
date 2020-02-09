acd3ad64813252cc40df32dd6c0078a4dadadd9d
Jayanth Puthalapattu <jayanthp17@gmail.com>
2020-02-09 09:05:27 +0530

    this is second commit

--- dem2/src/firstGIT.java
+++ dem2/src/firstGIT.java
@@ -4,7 +4,7 @@ public class firstGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println("This is first first git repo");
-  System.out.println("THis is second push");
+  System.out.println("THis is second commit");
 	}
 
 }
